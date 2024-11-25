b7e701871ac87db5bb21804bf1b7351bb4f8ac7e
Colin Blake <colinblake@gmail.com>
2024-11-25 13:08:50 -0500

    Add error checking Clean up logging

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getHumanChoice() {
-    let humanChoice = prompt("Rock, Paper, or Scissors?").toLowerCase()
-    console.log("Human choice:", humanChoice)
+    let humanChoice = null;
+    do{
+        humanChoice = prompt("Rock, Paper, or Scissors?").toLowerCase()
+    } while (!['rock', 'paper', 'scissors'].includes(humanChoice));
+
+    console.log("Human choice:", humanChoice);
     return humanChoice;
 }
 
@@ -67,13 +71,15 @@ function playRound(computerChoice, humanChoice){
             break;
     }
 
-    console.log(winner)
+    function capitalizeFirstLetter(val) {
+        return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+    }
 
     if (winner === 'human') {
-            console.log(humanChoice, "beats", computerChoice, "! You win!")
+            console.log(capitalizeFirstLetter(humanChoice), "beats", computerChoice, "! You win!")
             humanScore++;
     } else if (winner === 'computer') {
-            console.log(computerChoice, "beats", humanChoice, "! You lose!")
+            console.log(capitalizeFirstLetter(computerChoice), "beats", humanChoice, "! You lose!")
             computerScore++
     } else {
             console.log("Tie!")
@@ -85,10 +91,17 @@ function playRound(computerChoice, humanChoice){
 }
 
 for (round = 0; round < 5; round++){
+    console.log("*** Round " + (round+1) + " ***");
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
     playRound(computerSelection, humanSelection)
     console.log("Score: human " + humanScore + ", computer " + computerScore)
 }
 
-console.log('Finished!')
+if (humanScore > computerScore){
+    console.log("*** Human wins! ***");
+} else if (computerScore > humanScore){
+    console.log("*** Computer wins! ***");
+} else {
+    console.log('*** We have a tie! ***' );
+}
